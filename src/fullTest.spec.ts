6b87a5cd382ebafe0ed837ff713f633bb41db1cc
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-05-24 15:12:08 -0400

    ci(tests): refactor fullTest.spec.ts to use afterEach and promises for event handling

--- src/fullTest.spec.ts
+++ src/fullTest.spec.ts
@@ -14,30 +14,35 @@ describe("FullTest", () => {
 		throw new Error(`Environment variable ${name} is not set`);
 	}
 
-	afterAll(async () => {
+	afterEach(async () => {
 		if (connection) {
 			await connection.stopAsync();
 		}
 	});
 
-	it("should receive basic events", done => {
-		connection = ConnectionBuilder
-			.new({
-				applicationId: getEnvironmentVariable("APPLICATION_ID"),
-				discordToken: getEnvironmentVariable("DISCORD_TOKEN"),
-				publicKey: getEnvironmentVariable("PUBLIC_KEY")
-			})
-			.addDispatchEventHandler(EventName.Ready, payload => {
-				expect(payload.d?.user.id).toBeDefined();
+	it("should receive basic events", () => {
+		return new Promise<void>((resolve, reject) => {
+			connection = ConnectionBuilder
+				.new({
+					applicationId: getEnvironmentVariable("APPLICATION_ID"),
+					discordToken: getEnvironmentVariable("DISCORD_TOKEN"),
+					publicKey: getEnvironmentVariable("PUBLIC_KEY")
+				})
+				.addDispatchEventHandler(EventName.Ready, payload => {
+					try {
+						expect(payload.d?.user.id).toBeDefined();
+						resolve();
+					} catch (error: unknown) {
+						reject(error);
+					}
 
-				done();
+					return Promise.resolve();
+				})
+				.build();
 
-				return Promise.resolve();
-			})
-			.build();
-
-		connection
-			.startAsync()
-			.catch(done);
-	});
+			connection
+				.startAsync()
+				.catch(reject);
+		});
+	}, 5000);
 });
