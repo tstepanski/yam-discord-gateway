66924446541da12f78a2326dcb887a043d04459a
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-05-28 21:53:13 -0400

    feat(events): add GuildSoundboardSoundsUpdate interface and export from index
    Signed-off-by: Timothy M. Stepanski <timothy@timothystepanski.com>

--- src/types/soundboard/index.ts
+++ src/types/soundboard/index.ts
@@ -1,4 +1,5 @@
 export * from "./guildSoundboardSoundDelete";
+export * from "./guildSoundboardSoundsUpdate";
 export * from "./requestSoundboardSounds";
 export * from "./soundboardSound";
 export * from "./soundboardSoundsEvent";
