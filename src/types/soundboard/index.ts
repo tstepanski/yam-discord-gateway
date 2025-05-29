6f3d21c8eaa9081fc672fbbb813967a0493ea69e
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-05-28 21:56:30 -0400

    feat(events): add SoundboardSounds interface and export from index
    Signed-off-by: Timothy M. Stepanski <timothy@timothystepanski.com>

--- src/types/soundboard/index.ts
+++ src/types/soundboard/index.ts
@@ -2,5 +2,6 @@ export * from "./guildSoundboardSoundDelete";
 export * from "./guildSoundboardSoundsUpdate";
 export * from "./requestSoundboardSounds";
 export * from "./soundboardSound";
+export * from "./soundboardSounds";
 export * from "./soundboardSoundsEvent";
 export * from "./voiceChannelSoundEffectSend";
